3357bdab3a2d69f5d0235903246f958a5037a238
Kendal Katherine <kendal.katherine3@gmail.com>
2023-05-15 20:09:09 -0300

    first commit

---
+++ GAME1/src/main/java/com/mycompany/game1/GAME1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.game1;
+
+/**
+ *
+ * @author KENDAL
+ */
+public class GAME1 {
+
+    public static void main(String[] args) {
+    
+    }
+}
